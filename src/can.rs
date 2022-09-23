f70f5fca5b8783487e21be6a946cc15a85162ea9
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-09-22 23:01:44 -0400

    Move stuff

---
+++ src/can.rs
@@ -0,0 +1,55 @@
+fn python_capital_bool(b: bool) -> String {
+    (if b { "True" } else { "False" }).to_string()
+}
+
+#[derive(Clone)]
+pub struct CANValueType {
+    pub length: i32,
+    pub signed: bool,
+}
+
+impl CANValueType {
+    pub fn human_description(&self) -> String {
+        format!("{}{}", (if self.signed { "s" } else { "u" }), self.length)
+    }
+}
+
+#[derive(Clone)]
+pub struct CANSignal {
+    pub offset: i32,
+    pub name: String,
+
+    pub value_type: CANValueType,
+}
+
+impl CANSignal {
+    pub fn human_description(&self) -> String {
+        format!(
+            "Signal `{}`:\
+               \n -> offset: {},\
+               \n -> type: {}",
+            self.name,
+            self.offset,
+            self.value_type.human_description()
+        )
+    }
+
+    pub fn cantools_description(&self) -> String {
+        format!(
+            "cantools.database.can.Signal(name = '{name}',
+                             start = {offset},
+                             length = {length},
+                             is_signed = {signed})",
+            name = self.name,
+            offset = self.offset,
+            length = self.value_type.length,
+            signed = python_capital_bool(self.value_type.signed),
+        )
+    }
+}
+
+pub struct CANMessage {
+    pub name: String,
+
+    pub signals: Vec<CANSignal>,
+}
