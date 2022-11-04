0d80241546a6dace25f7012b571c2df5665cc587
Dan Mezhiborsky <dmezh@ieee.org>
2022-11-04 14:45:40 -0400

    Use thiserror

--- can/src/error.rs
+++ can/src/error.rs
@@ -1,30 +1,19 @@
-#[derive(Debug)]
-pub enum CANConstructionError {
-    SignalSpecifiedMultipleTimes(String),
-    MessageNameAlreadyExists(String),
-    MessageIdAlreadyExists(u32),
-    MessageNameInvalidChar(String, char),
-    MessageNameEmpty,
-}
+use thiserror::Error;
 
-impl std::fmt::Display for CANConstructionError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SignalSpecifiedMultipleTimes(s) => {
-                write!(f, "Signal with name `{s}` specified multiple times.")
-            }
-            Self::MessageNameAlreadyExists(n) => {
-                write!(f, "Message with name `{n}` already exists in network.")
-            }
-            Self::MessageIdAlreadyExists(i) => {
-                write!(f, "Message with id 0x{:x} already exists in network.", i)
-            }
-            Self::MessageNameInvalidChar(s, c) => {
-                write!(f, "Message name `{s}` includes invalid character `{c}`.")
-            }
-            Self::MessageNameEmpty => {
-                write!(f, "Message name is empty.")
-            }
-        }
-    }
+#[derive(Debug, Error)]
+pub enum CANConstructionError {
+    #[error("Signal with name `{0}` specified multiple times.")]
+    SignalSpecifiedMultipleTimes(String),
+
+    #[error("Message with name `{0}` already exists in network.")]
+    MessageNameAlreadyExists(String),
+
+    #[error("Message with id 0x{:x} already exists in network.", .0)]
+    MessageIdAlreadyExists(u32),
+
+    #[error("Message name `{0}` includes invalid character `{1}`.")]
+    MessageNameInvalidChar(String, char),
+
+    #[error("Message name is empty.")]
+    MessageNameEmpty,
 }
