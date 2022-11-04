541121757e2d8240992db9b2b11b829baf6e48fc
Dan Mezhiborsky <dmezh@ieee.org>
2022-11-04 14:30:43 -0400

    Refactor Message name checks and test

--- can/src/error.rs
+++ can/src/error.rs
@@ -3,14 +3,15 @@ pub enum CANConstructionError {
     SignalSpecifiedMultipleTimes(String),
     MessageNameAlreadyExists(String),
     MessageIdAlreadyExists(u32),
-    MessageNameInvalidChar(String),
+    MessageNameInvalidChar(String, char),
+    MessageNameEmpty,
 }
 
 impl std::fmt::Display for CANConstructionError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::SignalSpecifiedMultipleTimes(s) => {
-                write!(f, "Signal with name {s} specified multiple times.")
+                write!(f, "Signal with name `{s}` specified multiple times.")
             }
             Self::MessageNameAlreadyExists(n) => {
                 write!(f, "Message with name `{n}` already exists in network.")
@@ -18,9 +19,11 @@ impl std::fmt::Display for CANConstructionError {
             Self::MessageIdAlreadyExists(i) => {
                 write!(f, "Message with id 0x{:x} already exists in network.", i)
             }
-            // todo: Make description better to pinpoint invalid character
-            Self::MessageNameInvalidChar(s) => {
-                write!(f, "Message name `{s}` includes invalid character")
+            Self::MessageNameInvalidChar(s, c) => {
+                write!(f, "Message name `{s}` includes invalid character `{c}`.")
+            }
+            Self::MessageNameEmpty => {
+                write!(f, "Message name is empty.")
             }
         }
     }
