735cca8515ad416eb3396f0abf649c637b0cc0d9
Dan Mezhiborsky <dmezh@ieee.org>
2022-11-06 21:12:42 -0500

    More complete can.yml parse

--- cancompose/src/main.rs
+++ cancompose/src/main.rs
@@ -1,47 +1,63 @@
-use std::{collections::HashMap, iter::Map};
+use std::{collections::HashMap};
 
 use indoc::indoc;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
-struct JSignal {
-    #[serde(default)]
-    name: String,
-    scale: f32,
+#[serde(untagged)]
+enum JEnumeratedValue {
+    Auto(String),
+    Exact(u32),
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct JMessage {
+#[serde(deny_unknown_fields)]
+struct JSignal {
+    scale: f32,
+
     #[serde(default)]
-    name: String,
-    signals: Vec<HashMap<String, JSignal>>,
+    unit: Option<String>,
+
+    #[serde(default)]
+    enumerated_values: Vec<HashMap<String, JEnumeratedValue>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(deny_unknown_fields)]
+struct JMessage {
+    id: u32,
+
+    #[serde(default)]
+    cycletime_ms: Option<f32>,
+
+    #[serde(with = "tuple_vec_map")]
+    signals: Vec<(String, JSignal)>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JDesc {
+    #[serde(with = "tuple_vec_map")]
+    #[serde(flatten)]
+    messages: Vec<(String, JMessage)>
 }
 
 fn main() {
     let input = indoc! {r#"
-    - BRAKE_BrakeData:
-        signals:
-          - brakePressure:
-              scale: 0.5
-          - brakePercent:
-              scale: 0.01
-    - BRAKE_BrakeData:
-        signals:
-          - brakePressure:
-              scale: 0.5
-          - brakePercent:
-              scale: 0.01
-   "#};
-    let de: Vec<HashMap<String, JMessage>> = serde_yaml::from_str(&input).unwrap();
-    for mut msg in de {
-        let mut c: Vec<JMessage> = msg
-            .into_iter()
-            .map(|(k, mut v)| {
-                v.name = k;
-                v
-            })
-            .collect();
+    BRAKE_BrakeData:
+        id: 0x100
+        cycletime_ms: 1
 
-        println!("{:#?}", c);
-    }
+        signals:
+          brakePressure:
+            scale: 0.5
+          brakePercent:
+            scale: 0.01
+            unit: "%"
+            enumerated_values:
+              - SNA: auto
+              - SATURATED: 1
+    "#};
+    let de: JDesc = serde_yaml::from_str(&input).unwrap();
+
+    println!("{de:#?}");
 }
