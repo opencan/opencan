d30c1eba22a31ef4254cca112cb39b16175f65c4
Dan Mezhiborsky <dmezh@ieee.org>
2022-11-13 11:44:00 -0500

    Switch to derive_builder for Signal

--- can/src/signal.rs
+++ can/src/signal.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
-use typed_builder::TypedBuilder;
+use derive_builder::Builder;
 
 use crate::error::*;
 
-#[derive(Serialize, Deserialize, Clone, TypedBuilder)]
-#[builder(build_method(vis="", name=__build))]
+#[derive(Serialize, Deserialize, Clone, Builder)]
+#[builder(build_fn(name = "__build", error = "CANConstructionError", private))]
 pub struct CANSignal {
     pub name: String,
     pub start_bit: u32,
@@ -20,21 +20,19 @@ pub struct CANSignal {
     pub scale: Option<f32>,
 }
 
-// The signature looks scary, but we need this to implement our checked build()
-// on CANSignalBuilder. You can get the signature from `cargo expand`.
-#[allow(non_camel_case_types)]
-impl<
-        __scale: CANSignalBuilder_Optional<Option<f32>>,
-        __offset: CANSignalBuilder_Optional<Option<f32>>,
-        __description: CANSignalBuilder_Optional<Option<String>>,
-    > CANSignalBuilder<((String,), (u32,), (u32,), __description, __offset, __scale)>
-{
-    pub fn build(self) -> Result<CANSignal, CANConstructionError> {
-        let s = self.__build();
+impl CANSignalBuilder {
+    pub fn build(&self) -> Result<CANSignal, CANConstructionError> {
+        let s = self.__build()?;
         if s.width == 0 {
             return Err(CANConstructionError::SignalHasZeroWidth(s.name));
         }
 
         Ok(s)
+    }
+}
+
+impl CANSignal {
+    pub fn builder() -> CANSignalBuilder {
+        CANSignalBuilder::default()
     }
 }
