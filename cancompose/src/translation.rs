ba2e99d7d53049d3ce46451567618872dbc77c70
Dan Mezhiborsky <dmezh@ieee.org>
2022-11-14 03:42:08 -0500

    Split off make_sig in cancompose translation

--- cancompose/src/translation.rs
+++ cancompose/src/translation.rs
@@ -8,40 +8,45 @@ impl YDesc {
         let mut net = CANNetwork::new();
 
         for (msg_name, msg) in self.messages {
-            let mut sigs = Vec::new();
-
-            for (sig_name, sdesc) in msg.signals {
-                let context =
-                    || format!("Could not create signal `{sig_name} while building `{msg_name}`");
-
-                let mut new_sig = CANSignal::builder()
-                    .name(sig_name.clone())
-                    .start_bit(sdesc.start_bit)
-                    .description(sdesc.description)
-                    .scale(sdesc.scale);
-
-                if let Some(w) = sdesc.width {
-                    new_sig = new_sig.width(w);
-                } else {
-                    new_sig = new_sig.infer_width_strict().context(context())?;
-                }
-
-                let sig = new_sig.build().context(context())?;
-                sigs.push(sig);
-            }
-
-            let can_msg = CANMessage::builder()
+            let mut can_msg = CANMessageBuilder::default()
                 .name(msg_name.clone())
                 .id(msg.id)
-                .cycletime_ms(msg.cycletime_ms)
-                .signals(sigs)
+                .cycletime_ms(msg.cycletime_ms);
+
+            for (sig_name, sdesc) in msg.signals {
+                let sig = Self::make_sig(&sig_name, sdesc).context(format!(
+                    "Could not create signal `{sig_name} while building `{msg_name}`"
+                ))?;
+
+                can_msg = can_msg.add_signal(sig).context(format!(
+                    "Could not add signal `{sig_name}` to message `{msg_name}`"
+                ))?;
+            }
+
+            let m = can_msg
                 .build()
                 .context(format!("Could not create message `{msg_name}`"))?;
 
-            net.insert_msg(can_msg)
+            net.insert_msg(m)
                 .context(format!("Could not insert message `{msg_name}`"))?;
         }
 
         Ok(net)
+    }
+
+    fn make_sig(sig_name: &str, sdesc: YSignal) -> Result<CANSignal, CANConstructionError> {
+        let mut new_sig = CANSignal::builder()
+            .name(sig_name.into())
+            .start_bit(sdesc.start_bit) // the answer is that start_bit should be in the MESSAGE!
+            .description(sdesc.description)
+            .scale(sdesc.scale);
+
+        if let Some(w) = sdesc.width {
+            new_sig = new_sig.width(w);
+        } else {
+            new_sig = new_sig.infer_width_strict()?;
+        }
+
+        Ok(new_sig.build()?)
     }
 }
