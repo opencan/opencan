587aeaca6420642b12196e3e8ca1fc9022a9534c
Dan Mezhiborsky <dmezh@ieee.org>
2022-11-17 01:40:54 -0500

    WIP tasty

--- can/src/message.rs
+++ can/src/message.rs
@@ -7,6 +7,8 @@ use serde::{Deserialize, Serialize};
 use crate::error::*;
 use crate::signal::*;
 
+const MAX_MESSAGE_BIT: u32 = 63;
+
 #[derive(Serialize, Deserialize, Clone)]
 pub struct CANSignalWithPosition {
     // todo: make it pub(crate) and have APIs expose tuple (bit, sig)?
@@ -32,6 +34,9 @@ pub struct CANMessage {
     #[builder(setter(custom), field(type = "HashMap<String, usize>"))]
     #[serde(skip)]
     pub sig_map: HashMap<String, usize>,
+
+    #[builder(setter(custom), field(type = "u32"))]
+    pub length: u32,
 }
 
 impl CANMessageBuilder {
@@ -39,8 +44,17 @@ impl CANMessageBuilder {
     /// Message names must be at least one character long and must contain
     /// only ASCII letters, numbers, and underscores.
     // todo: check message ID validity and choose extended or non-extended
-    // todo: check that signals fit within message and do not overlap
-    pub fn build(self) -> Result<CANMessage, CANConstructionError> {
+    pub fn build(mut self) -> Result<CANMessage, CANConstructionError> {
+        // set message length in bytes to last signal's end position / 8
+        self.length = self.signals.last().map_or(0, |s| {
+            let end = s.bit + s.sig.width;
+            if (end % 8) == 0 {
+                end / 8
+            } else {
+                (end / 8) + 1
+            }
+        });
+
         let msg = self.__build()?;
 
         Self::check_name_validity(&msg.name)?;
@@ -48,19 +62,51 @@ impl CANMessageBuilder {
         Ok(msg)
     }
 
-    // /// Add single signal to message.
-    // /// See [`add_signal()`][CANMessageBuilder::add_signal_fixed()] for more details.
-    // pub fn add_signal(mut self, sig: CANSignal) -> Result<Self, CANConstructionError> {
-    //     todo!()
-    // }
+    /// Add single signal to message.
+    /// See [`add_signal_fixed()`][CANMessageBuilder::add_signal_fixed()] for more details.
+    pub fn add_signal(self, sig: CANSignal) -> Result<Self, CANConstructionError> {
+        let bit = self.signals.last().map_or(0, |s| s.bit + s.sig.width);
+
+        self.add_signal_fixed(bit, sig)
+    }
 
     /// Add single signal to message with signal position (start bit) specified.
     ///
     /// Checks:
     ///  - signal name does not repeat ([`SignalSpecifiedMultipleTimes`][CANConstructionError::SignalSpecifiedMultipleTimes])
-    pub fn add_signal_fixed(mut self, bit: u32, sig: CANSignal) -> Result<Self, CANConstructionError> {
+    ///  - signals in message do not overlap ([`SignalsOverlap`][CANConstructionError::SignalsOverlap])
+    ///  - signal does not extend past end of message (['SignalWillNotFitInMessage`][CANConstructionError::SignalWillNotFitInMessage])
+    pub fn add_signal_fixed(
+        mut self,
+        bit: u32,
+        sig: CANSignal,
+    ) -> Result<Self, CANConstructionError> {
         if self.sig_map.contains_key(&sig.name) {
             return Err(CANConstructionError::SignalNameAlreadyExists(sig.name));
+        }
+
+        // Check signal ranges don't overlap
+        // The signals are stored sorted by start bit in self.signals, so we
+        // only need to ensure the last signal's end bit is no later than this
+        // signal's start bit
+        if let Some(last) = self.signals.last() {
+            if bit <= (last.bit + last.sig.width - 1) {
+                return Err(CANConstructionError::SignalsOverlap(
+                    last.sig.name.clone(),
+                    sig.name,
+                    bit,
+                ));
+            }
+        }
+
+        // Check signal end position is not past the end of the message
+        let new_end = bit + sig.width - 1;
+        if new_end > MAX_MESSAGE_BIT {
+            return Err(CANConstructionError::SignalWillNotFitInMessage(
+                sig.name,
+                new_end,
+                MAX_MESSAGE_BIT,
+            ));
         }
 
         self.sig_map.insert(sig.name.clone(), self.signals.len());
@@ -69,22 +115,25 @@ impl CANMessageBuilder {
         Ok(self)
     }
 
-    // /// Add multiple signals to message, placing each signal's start position
-    // /// after the previous signal ends.
-    // ///
-    // /// Convenience wrapper for [`add_signal()`][CANMessageBuilder::add_signal].
-    // pub fn add_signals(mut self, sigs: Vec<CANSignal>) -> Result<Self, CANConstructionError> {
-    //     for sig in sigs {
-    //         self = self.add_signal(sig)?;
-    //     }
+    /// Add multiple signals to message, placing each signal's start position
+    /// after the previous signal ends.
+    ///
+    /// Convenience wrapper for [`add_signal()`][CANMessageBuilder::add_signal].
+    pub fn add_signals(mut self, sigs: Vec<CANSignal>) -> Result<Self, CANConstructionError> {
+        for sig in sigs {
+            self = self.add_signal(sig)?;
+        }
 
-    //     Ok(self)
-    // }
+        Ok(self)
+    }
 
     /// Add multiple signals to message with signal positions (start bits) specified.
     ///
     /// Convenience wrapper for [`add_signal_fixed()`][CANMessageBuilder::add_signal_fixed].
-    pub fn add_signals_fixed(mut self, sigs: Vec<(u32, CANSignal)>) -> Result<Self, CANConstructionError> {
+    pub fn add_signals_fixed(
+        mut self,
+        sigs: Vec<(u32, CANSignal)>,
+    ) -> Result<Self, CANConstructionError> {
         for (bit, sig) in sigs {
             self = self.add_signal_fixed(bit, sig)?;
         }
