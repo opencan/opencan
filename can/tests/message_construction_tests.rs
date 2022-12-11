340887417b8a164359df06c51846bc39bfcd06ff
Joya <97998493+poltergeistjoya@users.noreply.github.com>
2022-12-11 17:38:17 -0500

    Update can/tests/message_construction_tests.rs
    Co-authored-by: Dan Mezhiborsky <dmezh@ieee.org>
    Signed-off-by: Joya <97998493+poltergeistjoya@users.noreply.github.com>

--- can/tests/message_construction_tests.rs
+++ can/tests/message_construction_tests.rs
@@ -69,7 +69,6 @@ fn test_repeated_sig_name() {
 }
 
 ///  - signals are specified in order([`MessageSignalsOutOfOrder`][CANConstructionError::MessageSignalsOutOfOrder])
-//ok but CANSignal doesn't even have an option for startbit ??? in its struct so what ??
 #[test]
 fn test_sig_specified_in_order() {
     //try to make this into a closure or a function to make it easier
