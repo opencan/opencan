3f4f77f0be94534172ba1f38b06a76674a498568
Dan Mezhiborsky <dmezh@ieee.org>
2023-01-03 22:34:07 -0500

    Add get_ty_for_decoded_signal

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -17,6 +17,7 @@ pub enum CSignalTy {
     U16,
     U32,
     U64,
+    Float,
 }
 
 impl Display for CSignalTy {
@@ -29,6 +30,7 @@ impl Display for CSignalTy {
                 Self::U16 => "uint16_t",
                 Self::U32 => "uint32_t",
                 Self::U64 => "uint64_t",
+                Self::Float => "float", // todo: use a typedef?
             }
         )
     }
@@ -45,19 +47,31 @@ impl Codegen {
         for msg in node_msgs {
             // generate structs
             // ok, for this message, let's generate a struct for each signal
-            output += &format!("struct CAN_Message_{} {{\n", msg.name);
+            output += &format!("struct CAN_Message_{} {{", msg.name);
 
             for sigbit in &msg.signals {
-                output += &format!(
-                    "    // --- Signal: {}, offset: {}\n",
-                    sigbit.sig.name, sigbit.bit
-                );
+                output += "\n";
+
+                /* start comment block with signal name */
+                output += &format!("    /* --- Signal: {}\n", sigbit.sig.name);
+
+                /* description */
+                if let Some(d) = &sigbit.sig.description {
+                    output += "     *\n";
+                    output += &format!("     * ----> Description: \"{d}\"\n");
+                }
+
+                /* start bit */
+                output += &format!("     * ----> Start bit: {}\n", sigbit.bit);
+
+                /* finish comment block */
+                output += "     */\n";
 
                 output += &format!(
                     "    {} {};\n",
-                    Self::get_ty_for_signal(&sigbit.sig), sigbit.sig.name);
-
-                output += "\n";
+                    Self::get_ty_for_decoded_signal(&sigbit.sig),
+                    sigbit.sig.name
+                );
             }
 
             output += "};\n\n";
@@ -66,14 +80,30 @@ impl Codegen {
         Ok(output)
     }
 
-    fn get_ty_for_signal(sig: &CANSignal) -> CSignalTy {
+    /// Get the C type for the decoded signal, not taking into account minimum/maximum capping.
+    /// This is always float right now.
+    fn get_ty_for_decoded_signal(_sig: &CANSignal) -> CSignalTy {
+        // todo: need integer signal bounds support
+        // should we make this implicit or explicit... hmmm...
+        // making it implicit (i.e. say 1 instead of 1.0) might be obtuse / ambiguous
+        // -> otoh, saying force_integer: yes or force_float: yes all the time is annnoying
+
+        // I think I lean implicit. The problem is then it becomes a nightmare in Rust code....
+
+        CSignalTy::Float
+    }
+
+    fn _get_ty_for_raw_signal(sig: &CANSignal) -> CSignalTy {
         // this is totally wrong, what a fail
         match sig.width {
             1..=8 => CSignalTy::U8,
             ..=16 => CSignalTy::U16,
             ..=32 => CSignalTy::U32,
             ..=64 => CSignalTy::U64,
-            w => panic!("Unexpectedly wide signal: `{}` is `{}` bits wide", sig.name, w)
+            w => panic!(
+                "Unexpectedly wide signal: `{}` is `{}` bits wide",
+                sig.name, w
+            ),
         }
     }
 }
