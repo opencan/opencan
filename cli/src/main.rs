d6bb64ebdac6e0100ddf91afaa6b719d0f9c93d6
Dan Mezhiborsky <dmezh@ieee.org>
2023-01-17 10:35:13 -0500

    Save codegen output to actual files in cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,9 @@
-use anyhow::Result;
+use std::fs::{create_dir, write};
+use std::path::Path;
+
+use anyhow::{anyhow, Result};
 use clap::Parser;
-use opencan_codegen::Codegen;
+use opencan_codegen::{Codegen, CodegenOutput};
 
 #[derive(clap::Parser)]
 struct PrimaryArgs {
@@ -27,8 +30,28 @@ fn main() -> Result<()> {
             })?;
             let gen = Codegen::new(a, &net);
             let out = gen.network_to_c()?;
-            println!("HEADER:\n{}\n\nRX_C:\n{}", out.callbacks_h, out.rx_c);
+            save_codegen_files(&out, "./codegenoutput")?;
             Ok(())
         }
     }
+}
+
+/// Save output files from codegen to given path.
+fn save_codegen_files(cg: &CodegenOutput, path: impl AsRef<Path>) -> Result<()> {
+    let path = path.as_ref();
+
+    if !path.exists() {
+        create_dir(path)?;
+    } else if !path.is_dir() {
+        return Err(anyhow!(
+            "Can't save codegen output into `{}` - is not a directory",
+            path.display()
+        ));
+    }
+
+    for (name, content) in cg.as_list() {
+        write(path.join(name), content)?;
+    }
+
+    Ok(())
 }
