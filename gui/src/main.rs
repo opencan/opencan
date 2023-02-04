2eff51fdcef6ae98039477f049c93ad42b8d8abe
poltergeistjoya <joyaranidebi@gmail.com>
2023-02-04 16:41:00 -0500

    Circle

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,22 +1,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use anyhow::Result;
-use eframe::egui::{self, Slider};
-use socketcan::CANSocket;
+use eframe::egui::{self};
 
 fn main() -> Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    #[cfg(target_os = "linux")]
-    let can = Some(socketcan::CANSocket::open("can0")?);
-    #[cfg(target_os = "macos")]
-    let can = None;
-
-    let gui = Gui { can_adapter: can };
+    let gui = Gui { count: 0 };
 
     let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        initial_window_size: Some(egui::vec2(500.0, 500.0)),
         ..Default::default()
     };
 
@@ -30,15 +24,45 @@ fn main() -> Result<()> {
 }
 
 struct Gui {
-    can_adapter: Option<CANSocket>,
+    count: u32
 }
 
 impl eframe::App for Gui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical_centered(|ui| ui.heading("OpenCAN Loves You"));
-
-            let frame = self.can_adapter.unwrap().read_frame().unwrap();
+            ui.vertical_centered_justified(|ui| example_plot(ui));
         });
+
+        self.count += 1;
     }
+}
+
+fn example_plot(ui: &mut egui::Ui) -> egui::Response {
+    use egui::plot::{Line, PlotPoints};
+    let n = 128;
+    let line_points: PlotPoints = (0..=n)
+        .map(|i| {
+            let x = egui::remap(i as f64, 0.0..=n as f64, -2.0..=2.0);
+            [x, (4.0 - (x * x)).sqrt()]
+        })
+        .collect();
+    let line_points2: PlotPoints = (0..=n)
+        .map(|i| {
+            let x = egui::remap(i as f64, 0.0..=n as f64, -2.0..=2.0);
+            [x, -(4.0 - (x * x)).sqrt()]
+        })
+        .collect();
+    let line = Line::new(line_points);
+    let line2 = Line::new(line_points2);
+    egui::plot::Plot::new("example_plot")
+        .height(300.0)
+        .data_aspect(1.0)
+        .auto_bounds_x()
+        .auto_bounds_y()
+        .show(ui, |plot_ui| {
+            plot_ui.line(line);
+            plot_ui.line(line2);
+        })
+        .response
 }
