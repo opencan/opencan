71f9b1ff390e06d11650362da71c6d6c173c97c2
poltergeistjoya <joyaranidebi@gmail.com>
2023-02-17 13:59:54 -0500

    Reads bitrate from .yml

--- analyze/src/lib.rs
+++ analyze/src/lib.rs
@@ -11,12 +11,15 @@ impl<'n> Analyze<'n> {
     //https://electronics.stackexchange.com/questions/422998/how-to-calculate-bus-load-of-can-bus
     pub fn printnodes(&self) {
         let mut frames: u32 = 0;
-        let tbit = 1; // Mbit/s = 1 bit/microsecond = 1 MHz
-                      //for 29 bit id, we need something in the .yml or some other settings file to specify longer msg ids
-                      //i thought about reading the msg id with some function from compose, but that would require the existence of a msg with the highest id
-                      // im forcing it to 11 here
+        //for 29 bit id, we need something in the .yml or some other settings file to specify longer msg ids
+        //i thought about reading the msg id with some function from compose, but that would require the existence of a msg with the highest id
+        // im forcing it to 11 here
 
-        let cap = tbit * 1000000;
+        let Some(cap) = self.net.bitrate() else
+        {
+            eprintln!("No bitrate specified in network!");
+            std::process::exit(-1);
+        };
 
         let mut bits_sent = 0;
 
@@ -40,7 +43,7 @@ impl<'n> Analyze<'n> {
 
         let busload = ((bits_sent as f64) / (cap as f64)) * 100.0;
 
-        println!("Busload at {busload}%")
+        println!("Busload at {busload:.2}%")
     }
 }
 
