f9bcbf7b34acd8962f520ea94dd1c473e3658f2c
poltergeistjoya <joyaranidebi@gmail.com>
2023-02-17 17:40:42 -0500

    basic table

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -2,23 +2,26 @@
 
 use anyhow::Result;
 use eframe::egui::{self};
+use egui_extras::{Column, TableBuilder};
 
 fn main() -> Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    let gui = Gui { count: 0, table: Table{striped: true,resizeable:true} };
+    let gui = Gui {
+        count: 0,
+        table: Table {
+            striped: true,
+            resizeable: true,
+        },
+    };
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(1000.0, 1000.0)),
         ..Default::default()
     };
 
-    eframe::run_native(
-        "OpenCAN GUI",
-        options,
-        Box::new(|_cc| Box::new(gui)),
-    );
+    eframe::run_native("OpenCAN GUI", options, Box::new(|_cc| Box::new(gui)));
 
     Ok(())
 }
@@ -28,28 +31,46 @@ struct Gui {
     table: Table,
 }
 
-/// Table with dynamic layout 
-#[cfg_attr(feature = "serde", derive(serde::Deserialize, srde:: Serialize))]
-pub struct Table{
+/// Table with dynamic layout
+#[cfg_attr(feature = "serde", derive(serde::Deserialize, srde::Serialize))]
+pub struct Table {
     striped: bool,
     resizeable: bool,
 }
-
 
 impl eframe::App for Gui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical(|ui| {
-                ui.horizontal(|ui|{
-                    ui.checkbox(&mut self.table.striped, "Striped");
-                    ui.checkbox(&mut self.table.resizeable, "Resizeable Columns");
-                });
-
+                TableBuilder::new(ui)
+                    .striped(true)
+                    .resizable(true)
+                    .column(Column::auto().at_least(30.0).clip(true).resizable(true))
+                    .column(Column::auto().at_least(50.0).clip(true).resizable(true))
+                    .column(Column::remainder())
+                    .header(20.0, |mut header| {
+                        header.col(|ui| {
+                            ui.heading("ID");
+                        });
+                        header.col(|ui| {
+                            ui.heading("Name");
+                        });
+                        header.col(|ui| {
+                            ui.heading("Message");
+                        });
+                    })
+                    .body(|mut body| {
+                                let row_height = 40.0;
+                                 let num_rows = 2;
+                                 body.rows(row_height, num_rows, |row_index, mut row| {
+                                     row.col(|ui| {
+                                         ui.label("First column");
+                                     });
+                                 });
+                             });
             });
-
         });
 
         self.count += 1;
     }
 }
-
