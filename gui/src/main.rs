eeb18d1199068d68afc4fd23e0f5e45badc8ec52
Dan Mezhiborsky <dmezh@ieee.org>
2023-02-22 17:17:37 -0500

    Messages in GUI

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,14 +1,37 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::{sync::mpsc, ops::Deref, error, time::Duration};
+
 use anyhow::Result;
 use eframe::egui::{self};
 use egui_extras::{Column, TableBuilder};
+use pycanrs::{PyCanMessage, PyCanBusType};
+
+struct Gui {
+    messages: mpsc::Receiver<PyCanMessage>,
+    last_message: Option<PyCanMessage>,
+}
 
 fn main() -> Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    let gui = Gui { count: 0 };
+    let (tx, rx) = mpsc::channel();
+
+    // Set up CAN listener
+    let message_cb = move |msg: &PyCanMessage| {
+        tx.send(msg.clone()).unwrap()
+    };
+    let error_cb = |_: &_| {
+        eprintln!("Error from pycanrs!");
+        std::process::exit(-1);
+    };
+
+    // let can = pycanrs::PyCanInterface::new(PyCanBusType::Socketcand { host: "side".into(), channel: "vcan0".into(), port: 30000 })?;
+    let can = pycanrs::PyCanInterface::new(PyCanBusType::Slcan { bitrate: 500000, serial_port: "/dev/tty.usbmodem11201".into() } )?;
+    can.register_rx_callback(message_cb, error_cb)?;
+
+    let gui = Gui { messages: rx, last_message: None };
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(1000.0, 1000.0)),
@@ -18,10 +41,6 @@ fn main() -> Result<()> {
     eframe::run_native("OpenCAN GUI", options, Box::new(|_cc| Box::new(gui))).unwrap();
 
     Ok(())
-}
-
-struct Gui {
-    count: u32,
 }
 
 impl eframe::App for Gui {
@@ -47,16 +66,25 @@ impl eframe::App for Gui {
                     })
                     .body(|body| {
                         let row_height = 40.0;
-                        let num_rows = 2;
+                        let num_rows = 1;
                         body.rows(row_height, num_rows, |_row_index, mut row| {
+                            let msg = self.messages.recv_timeout(Duration::ZERO);
+                            if let Ok(m) = msg { self.last_message = Some(m) };
+
+                            let label = if let Some(m) = &self.last_message {
+                                format!("{m}")
+                            } else {
+                                format!("(none yet)")
+                            };
+
                             row.col(|ui| {
-                                ui.label("First column");
+                                ui.label(label);
                             });
                         });
                     });
             });
         });
 
-        self.count += 1;
+        ctx.request_repaint();
     }
 }
