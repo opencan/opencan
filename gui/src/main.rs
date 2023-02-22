7374b2fa7c421a60bd5d3f532e8003790239a4af
Dan Mezhiborsky <dmezh@ieee.org>
2023-02-22 17:48:09 -0500

    Use pycanrs 0.1.0 and remove socketcan dependency for GUI

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use std::{sync::mpsc, ops::Deref, error, time::Duration};
+use std::{sync::mpsc, time::Duration};
 
 use anyhow::Result;
 use eframe::egui::{self};
