63ddf45319c301ce6e5f268edec133a518d29ccc
Dan Mezhiborsky <dmezh@ieee.org>
2023-04-30 02:12:52 -0400

    Rename to Juice and recieve multiple messages with overwrite

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,15 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use std::{sync::mpsc, time::Duration};
+use std::{collections::BTreeMap, sync::mpsc};
 
 use anyhow::Result;
 use eframe::egui::{self};
 use egui_extras::{Column, TableBuilder};
-use pycanrs::{PyCanMessage, PyCanBusType};
+use pycanrs::{PyCanBusType, PyCanMessage};
 
 struct Gui {
     messages: mpsc::Receiver<PyCanMessage>,
-    last_message: Option<PyCanMessage>,
+
+    /// Message ID -> last data
+    message_history: BTreeMap<u32, PyCanMessage>,
 }
 
 fn main() -> Result<()> {
@@ -19,32 +21,42 @@ fn main() -> Result<()> {
     let (tx, rx) = mpsc::channel();
 
     // Set up CAN listener
-    let message_cb = move |msg: &PyCanMessage| {
-        tx.send(msg.clone()).unwrap()
-    };
+    let message_cb = move |msg: &PyCanMessage| tx.send(msg.clone()).unwrap();
     let error_cb = |_: &_| {
         eprintln!("Error from pycanrs!");
         std::process::exit(-1);
     };
 
-    // let can = pycanrs::PyCanInterface::new(PyCanBusType::Socketcand { host: "side".into(), channel: "vcan0".into(), port: 30000 })?;
-    let can = pycanrs::PyCanInterface::new(PyCanBusType::Slcan { bitrate: 500000, serial_port: "/dev/tty.usbmodem11201".into() } )?;
+    let can = pycanrs::PyCanInterface::new(PyCanBusType::Socketcand {
+        host: "side".into(),
+        channel: "vcan0".into(),
+        port: 30001,
+    })?;
+    // let can = pycanrs::PyCanInterface::new(PyCanBusType::Slcan { bitrate: 500000, serial_port: "/dev/tty.usbmodem11201".into() } )?;
     can.register_rx_callback(message_cb, error_cb)?;
 
-    let gui = Gui { messages: rx, last_message: None };
+    let gui = Gui {
+        messages: rx,
+        message_history: BTreeMap::new(),
+    };
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(1000.0, 1000.0)),
         ..Default::default()
     };
 
-    eframe::run_native("OpenCAN GUI", options, Box::new(|_cc| Box::new(gui))).unwrap();
+    eframe::run_native("Juice", options, Box::new(|_cc| Box::new(gui))).unwrap();
 
     Ok(())
 }
 
 impl eframe::App for Gui {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // drain messages from channel
+        while let Ok(msg) = self.messages.try_recv() {
+            self.message_history.insert(msg.arbitration_id, msg);
+        }
+
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.vertical(|ui| {
                 TableBuilder::new(ui)
@@ -66,21 +78,17 @@ impl eframe::App for Gui {
                     })
                     .body(|body| {
                         let row_height = 40.0;
-                        let num_rows = 1;
-                        body.rows(row_height, num_rows, |_row_index, mut row| {
-                            let msg = self.messages.recv_timeout(Duration::ZERO);
-                            if let Ok(m) = msg { self.last_message = Some(m) };
+                        let num_rows = self.message_history.len();
 
-                            let label = if let Some(m) = &self.last_message {
-                                format!("{m}")
-                            } else {
-                                format!("(none yet)")
-                            };
+                        body.rows(row_height, num_rows, |row_index, mut row| {
+                            let (_, msg) = self.message_history.iter().nth(row_index).unwrap();
+
+                            let label = msg.to_string();
 
                             row.col(|ui| {
                                 ui.label(label);
                             });
-                        });
+                        })
                     });
             });
         });
