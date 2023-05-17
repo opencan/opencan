3cfbe136e29f7043275569c14317c1b949507c0f
Jacob Koziej <jacobkoziej@gmail.com>
2023-05-17 12:26:27 -0400

    Define `message_ok_fn_def()`

--- codegen/src/message_ok.rs
+++ codegen/src/message_ok.rs
@@ -8,6 +8,8 @@ pub trait MessageStatusCodegen {
     fn message_ok_fn_name(&self, message: &CANMessage) -> String;
     /// Declaration of the `message_ok` function for the given RX message.
     fn message_ok_fn_decl(&self, message: &CANMessage) -> String;
+    /// Definition of the `message_ok` function for the given RX message.
+    fn message_ok_fn_def(&self, message: &CANMessage) -> String;
     /// Declarations of the `message_ok` functions for all of our RX messages.
     fn all_rx_messages_ok_fn_decls(&self) -> String;
     /// Definitions of the `message_ok` functions for all of our RX messages.
@@ -21,6 +23,48 @@ impl MessageStatusCodegen for Codegen<'_> {
 
     fn message_ok_fn_decl(&self, message: &CANMessage) -> String {
         format!("bool {}(void)", self.message_ok_fn_name(message))
+    }
+
+    fn message_ok_fn_def(&self, message: &CANMessage) -> String {
+        let Some(cycletime) = message.cycletime else {
+            return "// No message_ok() for this message without cycletime".into();
+        };
+
+        const TIME_TY: &str = "uint64_t";
+
+        let timestamp = formatdoc! {"
+            const {TIME_TY} timestamp = {};
+            ",
+            message.rx_timestamp_ident()
+        };
+        let time_check = formatdoc! {"
+            timestamp != 0U && (current_time - timestamp) <= (({cycletime}U * MS_TO_US) + LATENESS_TOLERANCE_US)",
+        };
+
+        let timestamp = timestamp.trim().indent(4);
+
+        // all together now
+        let check = formatdoc! {"
+            {decl} {{
+                // Check that message has been recieved (ever) + that it's on time.
+                const {TIME_TY} current_time = CAN_callback_get_system_time();
+                const uint_fast16_t MS_TO_US = 1000U;
+                const uint_fast16_t LATENESS_TOLERANCE_US = 100U;
+
+            {timestamp}
+
+                if (
+                    {time_check}
+                ) {{
+                    return true;
+                }}
+
+                return false;
+            }}\n\n",
+            decl = self.message_ok_fn_decl(&message),
+        };
+
+        check.trim().into()
     }
 
     fn all_rx_messages_ok_fn_decls(&self) -> String {
@@ -39,50 +83,21 @@ impl MessageStatusCodegen for Codegen<'_> {
     }
 
     fn all_rx_messages_ok_fn_defs(&self) -> String {
-        const TIME_TY: &str = "uint64_t";
+        let mut checks = self
+            .sorted_rx_messages
+            .iter()
+            .map(|msg| self.message_ok_fn_def(msg))
+            .collect::<Vec<_>>();
 
-        let mut checks = String::new();
+        // sort by message name
+        checks.sort();
 
-        for message in &self.sorted_rx_messages {
-            let Some(cycletime) = message.cycletime else {
-                continue; // just don't check this message
-            };
-
-            let timestamp = &formatdoc! {"
-                const {TIME_TY} timestamp = {};
-                ",
-                message.rx_timestamp_ident()
-            };
-            let check = &formatdoc! {"
-
-                timestamp != 0U && (current_time - timestamp) <= (({cycletime}U * MS_TO_US) + LATENESS_TOLERANCE_US) &&",
-            };
-
-            let timestamp = timestamp.trim().indent(4);
-            let check = check.strip_suffix("&&").unwrap().trim().indent(8);
-
-            // all together now
-            checks += &formatdoc! {"
-                {decl} {{
-                    // Check that message has been recieved (ever) + that it's on time.
-                    const {TIME_TY} current_time = CAN_callback_get_system_time();
-                    const uint_fast16_t MS_TO_US = 1000U;
-                    const uint_fast16_t LATENESS_TOLERANCE_US = 100U;
-
-                {timestamp}
-
-                    if (
-                {check}
-                    ) {{
-                        return true;
-                    }}
-
-                    return false;
-                }}\n\n",
-                decl = self.message_ok_fn_decl(&message),
-            }
-        }
-
-        checks.trim().into()
+        // collect into string with \n\n separators
+        checks
+            .into_iter()
+            .map(|def| def + "\n\n")
+            .collect::<String>()
+            .trim()
+            .into()
     }
 }
